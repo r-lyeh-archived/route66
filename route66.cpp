5fd0bf0b3adfdd40e4a6874c20011aeb4c7e38a8
r-lyeh <rlyeh.nospam@gmail.com>
2015-02-18 17:05:58 +0100

    warning fix;

--- route66.cpp
+++ route66.cpp
@@ -179,7 +179,7 @@ namespace {
     }
     bool match( const char *pattern, const char *str ) {
         if( *pattern=='\0' ) return !*str;
-        if( *pattern=='*' )  return match(pattern+1, str) || *str && match(pattern, str+1);
+        if( *pattern=='*' )  return match(pattern+1, str) || (*str && match(pattern, str+1));
         if( *pattern=='?' )  return *str && (*str != '.') && match(pattern+1, str+1);
         return (*str == *pattern) && match(pattern+1, str+1);
     }
